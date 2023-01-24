e73a3db407ee8cef3727bd41e628f9cc49361d5f
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-24 21:04:06 +0530

    Add files via upload

---
+++ Simpleif.java
@@ -0,0 +1,14 @@
+
+
+public class Simpleif {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        if(a+b < 35) {
+            System.out.println(" a+b is less than : 35 ");
+        }
+    }
+}
