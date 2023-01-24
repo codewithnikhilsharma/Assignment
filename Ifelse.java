1b95d6edd2e7d05962d43b9587d56dd066868d2f
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-24 21:14:04 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,17 @@
+
+
+public class Ifelse {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        if(a+b < 25) {
+            System.out.println(" a+b is less than : 25 ");
+
+        } else {
+            System.out.println( " a+b is greater than : 25 " );
+       }
+    }
+}
