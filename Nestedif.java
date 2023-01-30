ec35257f122761fb2829088880d3df7f364db2b9
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-30 10:09:42 +0530

    Add files via upload

---
+++ Nestedif.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Nestedif {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print(" Enter Your Age : ");
+        int age = in.nextInt();
+
+        if (age < 12) {
+            System.out.print(" You are a Child");
+        }
+
+       else if(age < 18) {
+            System.out.print(" You are a Teenager ");
+        }
+
+        if (age > 18) {
+            System.out.print(" You are a Adult ");
+        }
+
+    else if(age > 60) {
+            System.out.print(" You are a Senior-Citizen");
+        }
+    }
+}
