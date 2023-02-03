d6e84cd9519092bad1ea5bb5464962f0d6b1e860
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-03 09:21:41 +0530

    Add files via upload

---
+++ NaturalNo.java
@@ -0,0 +1,18 @@
+package controlStatement;
+
+public class NaturalNo {
+
+	public static void main(String[] args) {
+		
+		// sum of ten natural no
+	   int sum = 0;
+	    for (int i = 1; i <= 10; i++) {
+	      sum += i;
+	    }
+    System.out.println(" sum is : " + sum);
+		   }
+	 	}
+		
+          
+	        
+
