f0a55d849f536a352a842f1a20b602e0817b33e1
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-04 13:52:54 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,40 @@
+package Object;
+
+public class Operators {
+  
+	public static void main(String[] args) {
+		
+		
+			    int result = (101 + 0) / 3;
+			    System.out.println(  " (101 + 0) /3 : " +  result);
+			    
+			    double out = 3.0e-6 * 10000000.1;
+			    System.out.println(" 3.0e-6 * 10000000.1 : " + out);
+			    
+			
+			    	    boolean res = true && true;
+			    	    System.out.println(  "true && true : "  + res);
+			    	    
+			    	    boolean res1 = false && true;
+			    	    System.out.println( "false && true :" + res1);
+			    	    
+			    	    
+			    	    boolean res2 = false && false || true && true;
+			    	    System.out.println( " false && false || true && true :" + res2);
+			    	    
+			    	    boolean res3 = false || false && true && true;
+			    	    System.out.println( " false || false && true && true :" + res3);
+			    	    
+			    	    
+			    	       
+			    	  }
+			    	
+			      
+			      }
+			    
+			  	
+	       
+
+
+
+
