ef502fec468bed578dc93d5e7d75321363dc4df5
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-08 15:46:52 +0530

    Add files via upload

---
+++ Face.java
@@ -0,0 +1,14 @@
+package Today;
+
+public class Face {
+     
+	    public static void main(String[] args) {
+	        System.out.println(" +\"\"\"\"\"+");
+	        System.out.println("[| o o |]");
+	        System.out.println(" |  ^  |");
+	        System.out.println("  | '-' |");
+	        System.out.println(" +-----+");
+	    }
+	}
+
+
