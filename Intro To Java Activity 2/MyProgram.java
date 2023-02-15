cbdeb52a5cbcc33f6334c95d328339c99180a3d0
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-15 17:34:11 +0530

    Create MyProgram.java

---
+++ Intro To Java Activity 2/MyProgram.java
@@ -0,0 +1,16 @@
+package Today;
+
+
+class DisplayMessage {
+    void printMessage() {
+        System.out.println("Hello My Message");
+    }
+}
+
+public class MyProgram {
+  
+	 public static void main(String[] args) {
+	        DisplayMessage displayMessage = new DisplayMessage();
+	        displayMessage.printMessage();
+	    }
+}
