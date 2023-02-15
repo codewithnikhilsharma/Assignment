9098d3433aff8fd5035c1ff0032286421823c3e6
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-15 17:07:29 +0530

    Update AreaCalculator.java

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -1,5 +1,17 @@
-package Today;
+package com.cognizant.shapes;
 
-public class AreaCalculator {
-
+class Rectangle {
+    public void calculateArea(int length, int breadth) {
+        int area = length * breadth;
+        System.out.println("The Area of the rectangle is calculated using the formula length * breadth");
+    }
 }
+
+ class AreaCalculator {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.calculateArea(5, 10);
+    }
+}
+
+
